4e2a42c03915111651c6cfb12df6ec8a54e581cc
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-07 11:52:39 -0300

    add NextSeo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
+import { NextSeo } from 'next-seo';
 
 const Home = () => (
-  <main className="container">
-    <div>
-      <Image src="/pudim.jpg" alt="pudim" width="640px" height="480px" />
-      <address>
-        <a href="mailto:pudim@pudim.com.br">pudim@pudim.com.br</a>
-      </address>
-    </div>
-  </main>
+  <>
+    <NextSeo title="Home" />
+
+    <main className="container">
+      <div>
+        <Image src="/pudim.jpg" alt="pudim" width="640px" height="480px" />
+        <address>
+          <a href="mailto:pudim@pudim.com.br">pudim@pudim.com.br</a>
+        </address>
+      </div>
+    </main>
+  </>
 );
 
 export default Home;
