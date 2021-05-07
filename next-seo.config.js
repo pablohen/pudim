27df1bf66fcc04a72507648e74edcf3124e413db
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-07 10:52:38 -0300

    seo changes

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,23 @@
+const site = 'https://pudim.pablohen.vercel.app';
+const siteName = 'Pudim';
+const description = 'Uma homenagem a um dos sites mais antigos do Brasil';
+
 export default {
-  defaultTitle: 'Pudim',
-  titleTemplate: '%s | Pudim',
-  description: 'Uma homenagem a um dos sites mais antigos do Brasil',
+  defaultTitle: siteName,
+  titleTemplate: `%s | ${siteName}`,
+  description,
   openGraph: {
     type: 'website',
     locale: 'pt_BR',
-    site_name: 'Pudim',
+    site_name: siteName,
+    description,
+    images: [
+      {
+        url: `${site}/pudim.jpg`,
+        width: 640,
+        height: 480,
+        alt: 'Pudim',
+      },
+    ],
   },
 };
