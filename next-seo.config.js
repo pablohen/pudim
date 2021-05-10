0630723bfd2dda336ab8e1d58dabfffc334b7eac
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-10 08:29:27 -0300

    add keywords

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,11 +1,16 @@
+/* eslint-disable operator-linebreak */
 const site = `https://${process.env.VERCEL_URL}`;
 const siteName = 'Pudim';
-const description = 'Uma homenagem a um dos sites mais antigos do Brasil';
+const description =
+  'Uma homenagem a um dos sites mais antigos do Brasil, com algumas das melhores práticas disponíveis no mercado';
+const keywords =
+  'pudim, receita, lisinho, sem furinhos, leite condensado, leite em pó, tempo de preparo, maizena, air fryer, panela de pressão, forma, diabéticos, milho, brasil, brazil';
 
 export default {
   defaultTitle: siteName,
   titleTemplate: `%s | ${siteName}`,
   description,
+  keywords,
   openGraph: {
     type: 'website',
     locale: 'pt_BR',
